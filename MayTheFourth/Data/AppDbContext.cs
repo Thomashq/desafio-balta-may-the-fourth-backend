92c5541528f4acfda4cf18d4982ccc939409e6fe
Thomas Henrique Carvalho Pinheiro <thomashenriqq@gmail.com>
2024-04-21 12:35:24 -0300

    TASK1- Finalização da configuração do banco com migrations e adição dos pacotes NuGets básicos necessários

--- MayTheFourth/Data/AppDbContext.cs
+++ MayTheFourth/Data/AppDbContext.cs
@@ -1,22 +1,22 @@
->>>>>> feature/TASK1
-﻿using MayTheFourth.Models;
+using MayTheFourth.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace MayTheFourth.Data
 {
     public class AppDbContext : DbContext
     {
-        public DbSet<StarShip> StarShips{ get; set; }
+        public DbSet<StarShip> StarShip { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            BaseModel.Configure(builder);        
+            BaseModel.Configure(builder);
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlite("DataSource=app.db, Cache=Shared");
+            optionsBuilder.UseSqlite("DataSource=app.db;Cache=Shared");
         }
     }
+}
