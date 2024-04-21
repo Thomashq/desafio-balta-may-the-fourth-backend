dc7a5f787410ff4bb1478b1206756d4d7b615306
Thomas Henrique Carvalho Pinheiro <thomashenriqq@gmail.com>
2024-04-21 10:47:45 -0300

    TASK1- Correção da estrutura das pastas

---
+++ MayTheFourth/Models/BaseModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MayTheFourth.Models
+{
+    public abstract class BaseModel
+    {
+        public int Id { get; set; }
+
+        public static void Configure(ModelBuilder modelBuilder)
+        {
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                if (typeof(BaseModel).IsAssignableFrom(entityType.ClrType))
+                {
+                    var entity = modelBuilder.Entity(entityType.ClrType);
+                    entity.Property<long>("Id").HasColumnName($"{entityType.ClrType.Name}Id");
+                }
+            }
+        }
+
+    }
+}
