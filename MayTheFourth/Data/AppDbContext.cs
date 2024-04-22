1f4791d111726fecd1e482eba444b9824fd4355e
Thomas Henrique Carvalho Pinheiro <thomashenriqq@gmail.com>
2024-04-22 14:45:13 -0300

    TASK5: Adição da configuração dos nomes das colunas nas tabelas

--- MayTheFourth/Data/AppDbContext.cs
+++ MayTheFourth/Data/AppDbContext.cs
@@ -6,6 +6,7 @@ namespace MayTheFourth.Data
     public class AppDbContext : DbContext
     {
         public DbSet<StarShip> StarShip { get; set; }
+        public DbSet<Movie> Movie { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
---
+++ MayTheFourth/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MayTheFourth.Models
+{
+    public class Movie:BaseModel
+    {
+        [Column("name")]
+        public string Name { get; set; }
+    }
+}
