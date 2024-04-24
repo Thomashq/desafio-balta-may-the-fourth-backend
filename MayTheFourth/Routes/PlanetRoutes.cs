8503b0afc8ddb78910b377a15fe8ffd5a4d9751d
Thomas Henrique Carvalho Pinheiro <thomashenriqq@gmail.com>
2024-04-24 14:33:10 -0300

    TASK9- Adição da paginação

--- MayTheFourth/Routes/PlanetRoutes.cs
+++ MayTheFourth/Routes/PlanetRoutes.cs
@@ -1,6 +1,8 @@
 ﻿using MayTheFourth.Data;
 using MayTheFourth.Models;
 using MayTheFourth.Utility.Requests;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity;
 
 namespace MayTheFourth.Routes;
 
@@ -8,9 +10,9 @@ public static class PlanetRoutes
 {
 	public static void MapPlanetRoutes(this WebApplication app)
 	{
-		app.MapGet("/planets/{id}", (AppDbContext context, long id) =>
+		app.MapGet("/planets/{id:long}", async (AppDbContext context, long id) =>
 		{
-			var planet = context.Planet.Find(id);
+			var planet = await context.Planet.FindAsync(id);
 
 			if (planet is null)
 				return Results.NotFound("Planet could not be found.");
@@ -18,14 +20,27 @@ public static class PlanetRoutes
 			return Results.Ok(planet);
 		}).WithTags("Planets").WithSummary("Get planet by ID.").WithOpenApi();
 
-
-		app.MapGet("/planets/", (AppDbContext context) =>
+		app.MapGet("/planets/{skip:int}/{take:int}", async (AppDbContext context, int skip, int take) =>
 		{
-			var planets = context.Planet.ToList();
-			return Results.Ok(planets);
+			var totalCount = await context.Planet.CountAsync();
+
+			var planets = await context.Planet
+			.AsNoTracking()
+			.Skip(skip)
+			.Take(take)
+			.ToListAsync();
+
+			return Results.Ok(new
+			{
+				totalCount,
+				skip,
+				take,
+				data = planets
+			});
+
 		}).WithTags("Planets").WithSummary("Get all planets.").WithOpenApi();
 
-		app.MapPost("/planets/", (AppDbContext context, PlanetRequest planetRequest) =>
+		app.MapPost("/planets/", async (AppDbContext context, PlanetRequest planetRequest) =>
 		{
 			var planet = new Planet
 			{
@@ -42,35 +57,56 @@ public static class PlanetRoutes
 				Terrain = planetRequest.Terrain
 			};
 
-			context.Planet.Add(planet);
-			context.SaveChanges();
+			await context.Planet.AddAsync(planet);
+
+			await context.SaveChangesAsync();
+
 			return Results.Created($"/planets/{planet.Id}", planet.Id);
 		}).WithTags("Planets").WithSummary("Add new Planet.").WithOpenApi();
 
-		app.MapPut("/planets/{id}", (AppDbContext context, long id, PlanetRequest planetRequest) =>
+		app.MapPut("/planets/{id:long}", async (AppDbContext context, long id, PlanetRequest planetRequest) =>
 		{
-			var planetToEdit = context.Planet.Find(id);
+			var planetToEdit = await context.Planet.FindAsync(id);
 
 			if (planetToEdit is null)
 				return Results.NotFound("Could not find the planet");
 
 			planetToEdit.Update(planetRequest);
 
-			context.SaveChanges();
+			await context.SaveChangesAsync();
 
 			return Results.Ok("Planet edited successfully");
 		}).WithTags("Planets").WithSummary("Edit existing Planet.").WithOpenApi();
 
-		app.MapDelete("/planets/", (AppDbContext context, long id) =>
+		app.MapDelete("/planets/", async (AppDbContext context, long id) =>
 		{
-			var planetToDelete = context.Planet.Find(id);
+			var planetToDelete = await context.Planet.FindAsync(id);
 
 			if (planetToDelete is null)
 				return Results.NotFound($"Planet with id {id} does not exists");
 
 			context.Planet.Remove(planetToDelete);
-			context.SaveChanges();
+
+			await context.SaveChangesAsync();
+
 			return Results.Ok("Planet successfully deleted");
 		}).WithTags("Planets").WithSummary("Delete Planet by ID.").WithOpenApi();
+
+		app.MapDelete("/planets/multiDelete", async (AppDbContext context, [FromBody]IEnumerable<long> ids) =>
+		{
+			var planetsToDelete = await context.Planet
+			.Where(s => ids.Contains(s.Id))
+			.ToListAsync();
+
+			if (planetsToDelete.Count == 0)
+				return Results.NotFound("Could not find the planets");
+
+			context.Planet.RemoveRange(planetsToDelete);
+
+			await context.SaveChangesAsync();
+
+			return Results.Ok("Successfully deleted list of planets");
+
+		}).WithTags("Planets").WithSummary("Delete Multiple Planets").WithOpenApi();
 	}
 }
