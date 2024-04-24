64387eea60a34addc7bd95fcb658f0ec59b88d1d
Felipe Marques <felipe.comarques1208@gmail.com>
2024-04-24 00:25:20 -0300

    fix(TASK2): character table in database

--- MayTheFourth/Models/Character.cs
+++ MayTheFourth/Models/Character.cs
@@ -5,26 +5,26 @@ namespace MayTheFourth.Models
     public class Character : BaseModel
     {
         [Column("name")]
-        public string Name { get; set; } = string.Empty;
+        public string Name { get; set; }
         [Column("height")]
-        public string Height { get; set; } = string.Empty;
-        [Column("Weight")]
-        public string Weight { get; set; } = string.Empty;
-        [Column("HairColor")]
-        public string HairColor { get; set; } = string.Empty;
-        [Column("SkinColor")]
-        public string SkinColor { get; set; } = string.Empty;
-        [Column("EyeColor")]
-        public string EyeColor { get; set; } = string.Empty;
-        [Column("BirthYear")]
-        public string BirthYear { get; set; } = string.Empty;
-        [Column("Gender")]
-        public string Gender { get; set; } = string.Empty;
+        public string Height { get; set; }
+        [Column("weight")]
+        public string Weight { get; set; }
+        [Column("hairColor")]
+        public string HairColor { get; set; }
+        [Column("skinColor")]
+        public string SkinColor { get; set; }
+        [Column("eyeColor")]
+        public string EyeColor { get; set; }
+        [Column("birthYear")]
+        public string BirthYear { get; set; }
+        [Column("gender")]
+        public string Gender { get; set; }
         //[ForeignKey("Planet_Id")]
-        [Column("Planet")]
-        public string Planet { get; set; } = string.Empty;
+        [Column("planet")]
+        public string Planet { get; set; }
         //[ForeignKey("Movies_Id")]
-        [Column("Movies")]
-        public List<string> Movies { get; set; } = [];
+        [Column("movies")]
+        public List<string> Movies { get; set; }
     }
 }
