744b98b6efab2543c2d170362cd95d9d065592df
Felipe Marques <felipe.comarques1208@gmail.com>
2024-04-24 14:32:33 -0300

    feat(ISSUE2): update character 'put' route

--- MayTheFourth/Routes/CharacterRoutes.cs
+++ MayTheFourth/Routes/CharacterRoutes.cs
@@ -52,27 +52,33 @@ namespace MayTheFourth.Routes
                 
             }).WithTags("Characters").WithSummary("Create a character.").WithOpenApi();
 
-            app.MapPut("/characters/{id}", (AppDbContext context, long id, Character character) =>
+            app.MapPut("/characters/{id}", (AppDbContext context, long id, Character updatedCharacter) =>
             {
                 try
                 {
-                    var existingCharacter = context.Character.Find(id);
-                    if (existingCharacter is null)
-                    {
-                        return Results.NotFound();
-                    }
-                    existingCharacter.Name = character.Name;
-                    existingCharacter.Height = character.Height;
-                    existingCharacter.Weight = character.Weight;
-                    existingCharacter.HairColor = character.HairColor;
-                    existingCharacter.SkinColor = character.SkinColor;
-                    existingCharacter.EyeColor = character.EyeColor;
-                    existingCharacter.BirthYear = character.BirthYear;
-                    existingCharacter.Gender = character.Gender;
+                    Character characterToUpdate = context.Character.Find(id);
 
-                    context.Character.Update(existingCharacter);
+                    if (characterToUpdate is null)
+                    {
+                        return Results.NotFound($"Character with id {id} does not exist");
+                    }
+
+                    foreach (var property in typeof(Character).GetProperties())
+                    {
+                        var keyAttribute = property.GetCustomAttribute<KeyAttribute>();
+                        if (keyAttribute is not null)
+                            continue;
+
+                        var updatedValues = property.GetValue(updatedCharacter);
+                        if (updatedValues is null) 
+                            continue;
+
+                        property.SetValue(characterToUpdate, updatedValues);
+                    }
+
+                    context.Character.Update(characterToUpdate);
                     context.SaveChanges();
-                    return Results.Ok(existingCharacter);
+                    return Results.Ok($"Character with id {id} was successfully updated");
                 }
                 catch (Exception ex)
                 {
