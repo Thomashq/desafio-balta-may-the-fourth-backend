29ae8efbe04fb2479b5124397dd0e07952b024fa
Pedro Willian <pedrowillianpesquisador@gmail.com>
2024-04-29 14:13:27 -0300

    fix: removido código comentado da BaseModel

--- MayTheFourth/MayTheFourth.Domain/Models/BaseModel.cs
+++ MayTheFourth/MayTheFourth.Domain/Models/BaseModel.cs
@@ -7,17 +7,4 @@ public abstract class BaseModel
 {
     [Key]
     public long Id { get; set; }
-
-    //public static void Configure(ModelBuilder modelBuilder)
-    //{
-    //    foreach (var entityType in modelBuilder.Model.GetEntityTypes())
-    //    {
-    //        if (typeof(BaseModel).IsAssignableFrom(entityType.ClrType))
-    //        {
-    //            var entity = modelBuilder.Entity(entityType.ClrType);
-    //            entity.Property<long>("Id").HasColumnName($"{entityType.ClrType.Name}Id");
-    //        }
-    //    }
-    //}
-
 }
